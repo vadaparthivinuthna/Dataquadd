581a47f9ed952b56c0b8bd6622e185608bd808a7
VadaparthiVinuthna <vadaparthivinuthna@gmail.com>
2024-11-07 12:17:15 +0530

    committing all files

---
+++ Spring-inf/Spring-inf/src/main/java/com/example/Spring_inf/SpringInfApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_inf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInfApplication.class, args);
+	}
+
+}
